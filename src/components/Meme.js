d63a66a93d0d4e83d3787c2f9e5c00f5e8071499
Lawrence Ho <lawrence.jb.ho@gmail.com>
2022-05-08 12:49:42 -0700

    added data

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,21 @@
 import React from "react" 
-
+import memesData from "../memesData.js"
 
 function Meme() { 
+
+    function getMemesImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = (memesArray[randomNumber].url)
+        console.log(url)
+    }
+
     return (
         <main>
             <form className="form">
                 <input type="text" className="form-input" placeholder="top text"/>
                 <input type="text" className="form-input" placeholder="bottom text"/>
-                <button className="form-button">Get a new meme image</button>
+                <button className="form-button" onClick={getMemesImage}>Get a new meme image</button>
             </form>
         </main>
     )
