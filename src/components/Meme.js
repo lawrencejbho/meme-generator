375570deadba0f168462d4ea99652360e8840b72
Lawrence Ho <lawrence.jb.ho@gmail.com>
2022-05-15 18:25:34 -0700

    useEffect to fetch memesData using an API versus storing locally

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,21 +1,27 @@
-import React, {useState} from "react" 
-import memesData from "../memesData.js"
-
+import React, {useState, useEffect} from "react" 
 
 function Meme() { 
     
-     const [meme, setMeme] = React.useState({
+     const [meme, setMeme] = useState({
         topText: "",
         bottomText: "",
         randomImage: "http://i.imgflip.com/1bij.jpg" 
     })
 
-    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+    const [allMemes, setAllMemes] = useState([])
+
+    const [count, setCount] = useState({})
     
+    useEffect( () => {
+        console.log(allMemes)
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes))
+          }, []);
+
     function getMemeImage() {
-        const memesArray = allMemeImages.data.memes
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
         setMeme(prevMeme => ({
             ...prevMeme,
             randomImage: url
